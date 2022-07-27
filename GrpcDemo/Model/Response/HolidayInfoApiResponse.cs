67f641acedc0a91b837c1d0b73aea7cee352f7c4
sealongmaly <sealong.maly@yahoo.com>
2022-07-27 23:49:10 +0700

    rpc demo

---
+++ GrpcDemo/Model/Response/HolidayInfoApiResponse.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace GrpcDemo.Model.Response
+{
+    public class HolidayInfoApiResponse
+    {
+        [JsonProperty("date")]
+        public HolidayDateInfo Date { get; set; }
+
+        [JsonProperty("name")]
+        public List<HolidayNameInfo> Name { get; set; }
+
+        [JsonProperty("holidayType")]
+        public string HolidayType { get; set; }
+    }
+
+    public class HolidayNameInfo
+    {
+        [JsonProperty("lang")]
+        public string Language { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class HolidayDateInfo
+    {
+        [JsonProperty("day")]
+        public int Day { get; set; }
+
+        [JsonProperty("month")]
+        public int Month { get; set; }
+
+        [JsonProperty("year")]
+        public int Year { get; set; }
+        [JsonProperty("dayOfWeek")]
+        public int DayOfWeek { get; set; }
+    }
+}
---
+++ GrpcDemo/Services/HolidayInfoService.cs
@@ -0,0 +1,42 @@
+﻿using Google.Protobuf.Collections;
+using Grpc.Core;
+using GrpcDemo.Model.Response;
+using Newtonsoft.Json;
+
+namespace GrpcDemo.Services
+{
+    public class HolidayInfoService : HolidayInfo.HolidayInfoBase
+    {
+        private readonly IHttpClientService _httpClientService;
+        private readonly string _apiUrl;
+
+        public HolidayInfoService(IHttpClientService httpClientService, IConfiguration configuration)
+        {
+            _httpClientService = httpClientService;
+            _apiUrl = configuration.GetValue<string>("ApiUrl");
+        }
+        public override Task<HolidayInfoResponse> GetHoliday(HolidayInfoRequest request, ServerCallContext context)
+        {
+            var url = $"{_apiUrl}action=getHolidaysForYear&year={request.Year}&country={request.Country}&holidayType=all";
+            var apiResponse =  _httpClientService.Get(url);
+            var responseApi = JsonConvert.DeserializeObject<List<HolidayInfoApiResponse>>(apiResponse.Result)?.First();
+            var response = new HolidayInfoResponse()
+            {
+                //Date = new RepeatedField<HolidayInfoResponse.Types.dateInfo>(),
+                HolidayType = responseApi.HolidayType,
+
+            };
+            response.Date.Add(new HolidayInfoResponse.Types.dateInfo()
+            {
+                Day = responseApi.Date.Day,
+                Month = responseApi.Date.Month,
+                DayOfWeek = responseApi.Date.DayOfWeek,
+                Year = responseApi.Date.Year,
+
+            });
+
+            return Task.FromResult(response) ;
+
+        }
+    }
+}
